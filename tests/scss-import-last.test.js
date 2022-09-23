53dbc5184b8d52420d8a1e3d6ca6a91b31ccd691
Sebastian Nikkonen <sebastian.nikkonen@gmail.com>
2022-09-23 13:50:53 +0300

    Add tests

---
+++ tests/scss-import-last.test.js
@@ -0,0 +1,111 @@
+"use strict";
+
+const { RuleTester } = require('eslint');
+const scssImportLastRule = require('../rules/scss-import-last');
+
+const fixtures = {
+  valid: [
+    {
+      code: ''
+    },
+    {
+      code: "import a from './foo.js';"
+    },
+    {
+      code: (
+        "import a from './foo.js';" +
+        "import './style.scss';"
+      )
+    },
+    {
+      code: (
+        "import a from './foo.js';\n" +
+        "import b from './bar.js';\n" +
+        "import c from './baz.js';\n" +
+        "\n" +
+        "import './style.scss';"
+      ),
+    }
+  ],
+  invalid: [
+    {
+      code: (
+        "import './bbb.scss';\n" +
+        "import './ccc.scss';\n" +
+        "import a from './foo.js';\n" +
+        "import b from './bar.js';\n" +
+        "import c from './baz.js';\n" +
+        "import './aaa.scss';\n"
+      ),
+      output: (
+        "import './ccc.scss';\n" +
+        "import a from './foo.js';\n" +
+        "import b from './bar.js';\n" +
+        "import c from './baz.js';\n" +
+        "\n" +
+        "import './aaa.scss';\n" +
+        "import './bbb.scss';\n"
+      ),
+      errors: [
+        { message: 'SCSS import must be last: ./bbb.scss' },
+        { message: 'SCSS import must be last: ./ccc.scss' },
+      ],
+    },
+    {
+      code: (
+        "import './ccc.scss';\n" +
+        "import a from './foo.js';\n" +
+        "import b from './bar.js';\n" +
+        "import c from './baz.js';\n" +
+        "import './aaa.scss';\n" +
+        "import './bbb.scss';\n"
+      ),
+      output: (
+        "import a from './foo.js';\n" +
+        "import b from './bar.js';\n" +
+        "import c from './baz.js';\n" +
+        "\n" +
+        "import './aaa.scss';\n" +
+        "import './bbb.scss';\n" +
+        "import './ccc.scss';\n"
+      ),
+      errors: [
+        { message: 'SCSS import must be last: ./ccc.scss' },
+      ],
+    },
+    {
+      code: (
+        "import './aaa.scss';\n" +
+        "import a from './foo.js';\n" +
+        "import './bbb.scss';\n" +
+        "import b from './bar.js';\n" +
+        "import './ccc.scss';\n" +
+        "import c from './baz.js';\n"
+      ),
+      output: (
+        "import a from './foo.js';\n" +
+        "import './bbb.scss';\n" +
+        "import b from './bar.js';\n" +
+        "import './ccc.scss';\n" +
+        "import c from './baz.js';\n" +
+        "\n" +
+        "import './aaa.scss';\n"
+      ),
+      errors: [
+        { message: 'SCSS import must be last: ./aaa.scss' },
+        { message: 'SCSS import must be last: ./bbb.scss' },
+        { message: 'SCSS import must be last: ./ccc.scss' }
+      ],
+    },
+  ]
+}
+
+RuleTester.setDefaultConfig({
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 6,
+  }
+});
+
+const ruleTester = new RuleTester();
+ruleTester.run('scss-import-last', scssImportLastRule, fixtures);
